4b6c261eb3b08dc587f47d65552fc1e881c1a589
Stian Grenborgen <grenborgen.stian@nav.no>
2018-01-22 16:43:14 +0100

    Utvider skjemadefinisjon.

--- src/test/java/SoknadTest.java
+++ src/test/java/SoknadTest.java
@@ -1,35 +1,101 @@
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
 import com.fasterxml.jackson.databind.JsonNode;
 import com.github.fge.jackson.JsonLoader;
+import com.github.fge.jsonschema.core.exceptions.ProcessingException;
 import com.github.fge.jsonschema.core.report.ProcessingReport;
 import com.github.fge.jsonschema.main.JsonSchema;
 import com.github.fge.jsonschema.main.JsonSchemaFactory;
-import org.junit.Test;
 
-import java.nio.file.Paths;
-
-import static org.junit.Assert.assertEquals;
-
+@RunWith(Parameterized.class)
 public class SoknadTest {
+    
+    private static final String PREFIX_FIL_MED_FEIL = "feil-";
+    private static final String SCHEMA_FILE = "json/soknad/soknad.json";
+    private static final String TEST_DATA_DIRECTORY = "src/test/resources/json/soknad/";
 
-    JsonSchema validator;
+    private static JsonSchema validator = createValidator();
 
-    @Test
-    public void jsonValiderer() throws Exception {
-        JsonNode schemaResource = JsonLoader.fromFile(Paths.get("json/soknad/soknadSpesifikasjon.json").toFile());
-        validator = JsonSchemaFactory.byDefault().getJsonSchema(schemaResource);
+    private final File testfile; 
+
+    
+    public SoknadTest(String testName, File testfile) {
+        this.testfile = testfile;
+    }
+    
+    
+    @Parameters(name="{0}")
+    public static Iterable<Object[]> data() {
+        final List<File> testFiles = determineTestFiles(Paths.get(TEST_DATA_DIRECTORY).toFile());
         
-        valider("minimal", true);
-        valider("brukerdata", true);
-        
-        valider("feil-minimal-mangler-version", false);
+        return testFiles.stream()
+                .filter(ignoreTempFiles())
+                .map(toArrayWithName())
+                .collect(Collectors.toList());
     }
 
-    public void valider(String filnavn, boolean forventGyldig) throws Exception {
-        JsonNode json = JsonLoader.fromFile(Paths.get("src/test/resources/json/soknad/" + filnavn + ".json").toFile());
+    private static List<File> determineTestFiles(File parent) {
+        final List<File> result = new ArrayList<>();
+        if (parent.isDirectory()) {
+            for (File child : parent.listFiles()) {
+                result.addAll(determineTestFiles(child));
+            }
+        } else {
+            result.add(parent);
+        }
+        return result;
+    }
+
+
+    private static Function<? super File, ? extends Object[]> toArrayWithName() {
+        return f -> {
+            return new Object[] {
+                f.getName(), f
+            };
+        };
+    }
+
+
+    private static Predicate<? super File> ignoreTempFiles() {
+        return f -> !f.getName().startsWith("_");
+    }
+    
+    
+    @Test
+    public void jsonValiderer() throws Exception {
+        valider(testfile, !testfile.getName().startsWith(PREFIX_FIL_MED_FEIL));
+    }
+
+    public static void valider(File testfile, boolean forventGyldig) throws Exception {
+        JsonNode json = JsonLoader.fromFile(testfile);
 
         ProcessingReport report = validator.validate(json);
         System.out.println(report);
 
-        assertEquals("Fil " + filnavn + " forventes " + (forventGyldig ? "gyldig" : "ugyldig"), forventGyldig, report.isSuccess());
+        assertEquals("Fil " + testfile.getName() + " forventes " + (forventGyldig ? "gyldig" : "ugyldig") + "\n" + report, forventGyldig, report.isSuccess());
+    }
+    
+    private static JsonSchema createValidator() {
+        try {
+            final JsonNode schemaResource = JsonLoader.fromFile(Paths.get(SCHEMA_FILE).toFile());
+            return JsonSchemaFactory.byDefault().getJsonSchema(schemaResource);
+        } catch (IOException|ProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
