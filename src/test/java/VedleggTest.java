da94fa752efbf0fee2b7dc9b5699286b12887df5
Oskar Asplin <oskar.asplin@nav.no>
2018-11-28 10:07:56 +0100

    Endret til parametrisert test i VedleggTest

--- src/test/java/VedleggTest.java
+++ src/test/java/VedleggTest.java
@@ -2,25 +2,54 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import com.github.fge.jsonschema.core.report.ProcessingReport;
 
 import no.nav.sbl.soknadsosialhjelp.json.JsonSosialhjelpValidator;
 
+@RunWith(Parameterized.class)
 public class VedleggTest {
+
+    private final String navn;
+    private final boolean skalKjore;
+
+    
+    public VedleggTest(String navn, boolean skalkjore) {
+        this.navn = navn;
+        this.skalKjore = skalkjore;
+    }
 
     @Test
     public void jsonValiderer() throws Exception {
-        valider("gyldig_minimal", true);
-        valider("gyldig_minimal2", true);
-        valider("gyldig_standard", true);
-        valider("gyldig_ekstrafelter", true);
-        valider("ikkegyldig_ugyldigvedlegg", false);
-        valider("ikkegyldig_ugyldigfil", false);
+        valider(this.navn, this.skalKjore);
+    }
+    
+    @Parameters(name="{0}")
+    public static Iterable<Object[]> alleTester(){
+        return Arrays.asList(
+                testMed("gyldig_minimal", true),
+                testMed("gyldig_minimal2", true),
+                testMed("gyldig_standard", true),
+                testMed("gyldig_ekstrafelter", true),
+                testMed("ikkegyldig_ugyldigvedlegg", false),
+                testMed("ikkegyldig_ugyldigfil", false)
+                );
     }
 
+    private static Object[] testMed(String navn, boolean skalKjore) {
+        return new Object[] {
+            navn, skalKjore
+        };
+    }
     
     public void valider(String filnavn, boolean forventGyldig) {
         final File testfile = new File("src/test/resources/json/vedlegg/" + filnavn + ".json");
