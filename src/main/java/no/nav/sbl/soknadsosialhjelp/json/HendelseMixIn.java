55611efe1a0a28157a486fb1f39c923ed172b037
Jan Tore Bøe <jan.tore.boe@nav.no>
2019-05-03 15:00:46 +0200

    Revert "DIGISOS-1170 forslag til splitting av sokand"
    This reverts commit 95550dbc

--- src/main/java/no/nav/sbl/soknadsosialhjelp/json/HendelseMixIn.java
+++ src/main/java/no/nav/sbl/soknadsosialhjelp/json/HendelseMixIn.java
@@ -2,8 +2,8 @@ package no.nav.sbl.soknadsosialhjelp.json;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
 import no.nav.sbl.soknadsosialhjelp.digisos.soker.hendelse.JsonNyStatus;
-import no.nav.sbl.soknadsosialhjelp.digisos.soker.hendelse.JsonSaksStatus;
 import no.nav.sbl.soknadsosialhjelp.digisos.soker.hendelse.JsonTildeltNavKontor;
 import no.nav.sbl.soknadsosialhjelp.digisos.soker.hendelse.JsonVedtakFattet;
 
@@ -12,7 +12,6 @@ import no.nav.sbl.soknadsosialhjelp.digisos.soker.hendelse.JsonVedtakFattet;
 @JsonSubTypes({
     @JsonSubTypes.Type(value = JsonTildeltNavKontor.class, name = "tildeltNavKontor"),
     @JsonSubTypes.Type(value = JsonNyStatus.class, name = "nyStatus"),
-    @JsonSubTypes.Type(value = JsonVedtakFattet.class, name = "vedtakFattet"),
-    @JsonSubTypes.Type(value = JsonSaksStatus.class, name = "saksStatus")
+    @JsonSubTypes.Type(value = JsonVedtakFattet.class, name = "vedtakFattet")
 })
 public interface HendelseMixIn {}
