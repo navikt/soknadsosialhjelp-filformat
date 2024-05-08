8f3df3caf4f9778dd8cf1a8d02c9c067c758e9e5
Martin Almvik <martin.almvik@nav.no>
2024-05-08 15:54:39 +0200

    Flere tweaks på klage

--- src/main/java/no/nav/sbl/soknadsosialhjelp/json/KlageHendelseMixIn.java
+++ src/main/java/no/nav/sbl/soknadsosialhjelp/json/KlageHendelseMixIn.java
@@ -2,16 +2,16 @@ package no.nav.sbl.soknadsosialhjelp.json;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import no.nav.sbl.soknadsosialhjelp.digisos.soker.hendelse.JsonDokumentasjonkrav;
 import no.nav.sbl.soknadsosialhjelp.digisos.soker.klage.hendelse.JsonKlageStatus;
 import no.nav.sbl.soknadsosialhjelp.digisos.soker.klage.hendelse.JsonSaksfremlegg;
 import no.nav.sbl.soknadsosialhjelp.digisos.soker.klage.hendelse.JsonKlageUtfall;
 import no.nav.sbl.soknadsosialhjelp.digisos.soker.klage.hendelse.JsonStatsforvalterUtfall;
-import no.nav.sbl.soknadsosialhjelp.digisos.soker.klage.hendelse.JsonKlageDokumentasjonEtterspurt;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "type", visible = true)
 @JsonSubTypes({
     @JsonSubTypes.Type(value = JsonKlageStatus.class, name = "klageStatus"),
-    @JsonSubTypes.Type(value = JsonKlageDokumentasjonEtterspurt.class, name = "klageDokumentasjonEtterspurt"),
+    @JsonSubTypes.Type(value = JsonDokumentasjonkrav.class, name = "klageDokumentasjonEtterspurt"),
     @JsonSubTypes.Type(value = JsonKlageUtfall.class, name = "klageUtfall"),
     @JsonSubTypes.Type(value = JsonStatsforvalterUtfall.class, name = "statsforvalterUtfall"),
     @JsonSubTypes.Type(value = JsonSaksfremlegg.class, name = "saksfremlegg"),
