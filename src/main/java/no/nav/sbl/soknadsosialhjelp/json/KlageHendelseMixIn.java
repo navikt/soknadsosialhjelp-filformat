4cabd12e306ab527c4666a2a4d21a1f143115ae1
Martin <martinalmvik@gmail.com>
2024-05-08 09:47:33 +0200

    Legg til filformat for klage (#206)

---
+++ src/main/java/no/nav/sbl/soknadsosialhjelp/json/KlageHendelseMixIn.java
@@ -0,0 +1,21 @@
+package no.nav.sbl.soknadsosialhjelp.json;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import no.nav.sbl.soknadsosialhjelp.digisos.soker.klage.hendelse.JsonKlageStatus;
+import no.nav.sbl.soknadsosialhjelp.digisos.soker.klage.hendelse.JsonSaksfremlegg;
+import no.nav.sbl.soknadsosialhjelp.digisos.soker.klage.hendelse.JsonKlageUtfall;
+import no.nav.sbl.soknadsosialhjelp.digisos.soker.klage.hendelse.JsonStatsforvalterUtfall;
+import no.nav.sbl.soknadsosialhjelp.digisos.soker.klage.hendelse.JsonKlageDokumentasjonEtterspurt;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "type", visible = true)
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = JsonKlageStatus.class, name = "klageStatus"),
+    @JsonSubTypes.Type(value = JsonKlageDokumentasjonEtterspurt.class, name = "klageDokumentasjonEtterspurt"),
+    @JsonSubTypes.Type(value = JsonKlageUtfall.class, name = "klageUtfall"),
+    @JsonSubTypes.Type(value = JsonStatsforvalterUtfall.class, name = "statsforvalterUtfall"),
+    @JsonSubTypes.Type(value = JsonSaksfremlegg.class, name = "saksfremlegg"),
+})
+public interface KlageHendelseMixIn {
+
+}
