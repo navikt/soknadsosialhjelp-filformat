446a46ec8a6aaa16e669446800656bbda7c104c5
Amund Sørumshagen <amund.sorumshagen@nav.no>
2025-11-14 11:49:58 +0100

    Gradle publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
 }
 
-group = "no.nav.sbl.dialogarena"
+group = "no.nav.sosialhjelo.soknad"
 version = "1.0.0-SNAPSHOT"
 
 java {
