942b6b2d2dab28869117557ae716db767b1b2eac
Amund Sørumshagen <amund.sorumshagen@nav.no>
2026-02-04 07:10:05 +0100

    Oppdaterer deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,11 +38,11 @@ dependencies {
     implementation("com.google.guava:guava:33.5.0-jre")
     implementation("org.mozilla:rhino:1.9.0")
 
-    implementation("tools.jackson.core:jackson-databind:3.0.3")
+    implementation("tools.jackson.core:jackson-databind:3.0.4")
     implementation("com.fasterxml.jackson.core:jackson-annotations:2.21")
 
-    testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher:6.0.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:6.0.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:6.0.2")
     testImplementation("org.assertj:assertj-core:3.27.7")
 }
 
